92d82e4a3d9cdcde1724aa10971a119355720f0d
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-04 21:37:42 +0100

    Add acpi handler script

---
+++ etc/acpi/handler.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+# Default acpi script that takes an entry for all actions
+
+# NOTE: This is a 2.6-centric script.  If you use 2.4.x, you'll have to
+#       modify it to not use /sys
+
+minspeed=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq`
+maxspeed=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq`
+setspeed="/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed"
+
+set $*
+
+XDG_RUNTIME_DIR=$(find /run/user -maxdepth 1 -mindepth 1 -type d -print -quit)
+if ! [ -z "$XDG_RUNTIME_DIR" ]; then
+    WAYLAND_DISPLAY=$(find $XDG_RUNTIME_DIR -name "wayland-*" -type s -printf "%f" -quit)
+fi
+
+case "$1" in
+    button/power)
+        #echo "PowerButton pressed!">/dev/tty5
+        case "$2" in
+            PBTN|PWRF)
+		    logger "PowerButton pressed: $2, shutting down..."
+		    shutdown -P now
+		    ;;
+            *)      logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    button/sleep)
+        case "$2" in
+            SBTN|SLPB)
+		    # don't suspend-to-ram
+		    logger "Sleep Button pressed: $2"
+		    ;;
+            *)      logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    button/f20)
+	case "$2" in
+	    *)
+		MIC_BTN=/sys/devices/platform/thinkpad_acpi/leds/platform::micmute/brightness
+		[ -f $MIC_BTN ] || return
+		if [ $(cat $MIC_BTN) -eq 0 ]; then
+		    echo 1 > $MIC_BTN
+		    logger "Mic mute led on"
+		else 
+		    echo 0 > $MIC_BTN
+		    logger "Mic mute led off"
+		fi
+		;;
+	esac
+	;;
+    ac_adapter)
+        case "$2" in
+            AC|ACAD|ADP0)
+                case "$4" in
+                    00000000)
+                        echo -n $minspeed >$setspeed
+                        #/etc/laptop-mode/laptop-mode start
+                    ;;
+                    00000001)
+                        echo -n $maxspeed >$setspeed
+                        #/etc/laptop-mode/laptop-mode stop
+                    ;;
+                esac
+                ;;
+            *)  logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    battery)
+        case "$2" in
+            BAT0)
+                case "$4" in
+                    00000000)   #echo "offline" >/dev/tty5
+                    ;;
+                    00000001)   #echo "online"  >/dev/tty5
+                    ;;
+                esac
+                ;;
+            CPU0)
+                ;;
+            *)  logger "ACPI action undefined: $2" ;;
+        esac
+        ;;
+    button/lid)
+	case "$3" in
+		close)
+			# NOTE: would be nice if zzz worked on my thinkpad :(
+			logger "LID closed"
+			# lock screen
+			# FIXME: not sure why WAYLAND_DISPLAY is not exported
+			# Though I understand why XDG_RUNTIME_DIR is not
+			[ -z "$WAYLAND_DISPLAY" ] || runuser -l omar -c "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR WAYLAND_DISPLAY=$WAYLAND_DISPLAY setsid -f waylock"
+			# disable services
+			sv down iwd
+			sv down bluetoothd
+			# disable screen
+			[ -z "$WAYLAND_DISPLAY" ] || swaymsg output eDP-1 disable
+			;;
+		open)	
+			# enable laptop screen
+			logger "LID opened" 
+			# enable services again
+			sv up iwd
+			sv up bluetoothd
+			# enable screen
+			[ -z "$WAYLAND_DISPLAY"] || swaymsg output eDP-1 enable
+			;;
+		*) logger "ACPI action undefined (LID): $2";;
+	esac
+	;;
+    *)
+        logger "ACPI group/action undefined: $1 / $2"
+        ;;
+esac
